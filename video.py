854d81096a03e6a3364009f4a8f684aa7a5f3e1a
Zain Asif <98703238+ZainAsif767@users.noreply.github.com>
2022-12-22 22:31:17 +0500

    Delete video.py

--- video.py
+++
@@ -1,7 +0,0 @@
-# you need to imprt pytube library using --> pip install pytube (on TERMINAL)
-from pytube import YouTube
-url = input("Video URL: >")
-yt = YouTube(url)
-
-stream = yt.streams.first()
-stream.download()  # this will download in your current working directory
